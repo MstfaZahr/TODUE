2889c043bc17a9966a9570d6f1dab8df413ecc58
Yousri Ahmed <yousriahmed@hotmail.com>
2020-05-14 13:34:16 +0200

    added a login required decorator

--- application.py
+++ application.py
@@ -1,23 +1,26 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, url_for, request
+from functools import wraps
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def index():
-    return " "
-
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.user is None:
+            return redirect(url_for('signin', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
 
 @app.route("/sign-in")
 def signin():
     return render_template("sign-in.html")
-
 
 @app.route("/sign-up")
 def signup():
     return render_template("sign-up.html")
 
 @app.route("/todues")
+@login_required
 def todues_index():
     return render_template("todues.html")
 
